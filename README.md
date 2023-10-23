13b9e0fcda07e3e0b9649aa92d4da12a889b5ccc
Jora Troosh <tensorush@gmail.com>
2023-10-23 21:15:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## :lizard: :doughnut: **zig donut**
+
+[![CI][ci-shield]][ci-url]
+[![LC][lc-shield]][lc-url]
+
+### Zig port of [the animated ASCII-art donut rendering](https://gist.github.com/a1k0n/8ea6516b4946ab36348fb61703dc3194) created by [Andy Sloane](https://github.com/a1k0n).
+
+#### :rocket: Usage
+
+```sh
+git clone https://github.com/tensorush/zig-donut
+cd zig-donut/
+zig build exe
+```
+
+<!-- MARKDOWN LINKS -->
+
+[ci-shield]: https://img.shields.io/github/actions/workflow/status/tensorush/zig-donut/ci.yaml?branch=main&style=for-the-badge&logo=github&label=CI&labelColor=black
+[ci-url]: https://github.com/tensorush/zig-donut/blob/main/.github/workflows/ci.yaml
+[lc-shield]: https://img.shields.io/github/license/tensorush/zig-donut.svg?style=for-the-badge&labelColor=black
+[lc-url]: https://github.com/tensorush/zig-donut/blob/main/LICENSE.md
---
+++ build.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // Executable
+    const exe_step = b.step("exe", "Run Donut animated ASCII art rendering");
+
+    const exe = b.addExecutable(.{
+        .name = "donut",
+        .root_source_file = std.Build.FileSource.relative("src/main.zig"),
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+        .version = .{ .major = 1, .minor = 0, .patch = 0 },
+    });
+    b.installArtifact(exe);
+
+    const exe_run = b.addRunArtifact(exe);
+
+    exe_step.dependOn(&exe_run.step);
+    b.default_step.dependOn(exe_step);
+
+    // Lints
+    const lints_step = b.step("lint", "Run lints");
+
+    const lints = b.addFmt(.{
+        .paths = &.{ "src", "build.zig" },
+        .check = true,
+    });
+
+    lints_step.dependOn(&lints.step);
+    b.default_step.dependOn(lints_step);
+}
